55a2cb6d800d31a67e0af34b479bd05b57415773
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-04-25 17:37:39 +0300

    Initial Commit

---
+++ RandomFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomFood
+//
+//  Created by Kasharin Mikhail on 25.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
