5c69c54d47043004cf12f232990c4006acf212b4
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-04-26 14:46:40 +0300

    creat model and dataStore

---
+++ Receipt.swift
@@ -0,0 +1,34 @@
+//
+//  Receipt.swift
+//  RandomFood
+//
+//  Created by Kasharin Mikhail on 26.04.2023.
+//
+
+import Foundation
+
+struct Receipt {
+    let nameOfReceipt: String
+    let description: String
+    let ingredient: [String: String]
+    let calories: Int
+    let mealTime: MealTime
+    
+    
+    static func getReceipt(with mealTime: MealTime, calories: Int?) -> Receipt? {
+        if let calories {
+            return DataStore.shared.getReceipts()[mealTime]?
+                .filter{ ($0.calories - 100...$0.calories + 100).contains(calories)}
+                .randomElement()
+        } else {
+            return DataStore.shared.getReceipts()[mealTime]?
+                .randomElement()
+        }
+    }
+}
+
+enum MealTime {
+    case breakfast
+    case lunch
+    case dinner
+}
