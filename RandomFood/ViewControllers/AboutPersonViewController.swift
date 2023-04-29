248a0214afab60c126ad70d486e2bfa8ffd8cc56
aleksei <alikexie93@yandex.ru>
2023-04-29 16:47:09 +0600

    customize table cell

--- RandomFood/ViewControllers/AboutPersonViewController.swift
+++ RandomFood/ViewControllers/AboutPersonViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AboutPersonViewController: UIViewController {
+final class AboutPersonViewController: UIViewController {
     
     @IBOutlet var photoOfPerson: UIImageView!
     @IBOutlet var ageOfPerson: UILabel!
--- RandomFood/ViewControllers/InformationViewController.swift
+++ RandomFood/ViewControllers/InformationViewController.swift
@@ -7,18 +7,23 @@
 
 import UIKit
 
-class InformationViewController: UIViewController {
+final class InformationViewController: UIViewController {
     
+    //MARK: - IBOutlets
     @IBOutlet var tableView: UITableView!
     
+    //MARK: - Privat Properties
     private var teamList = PersonStore.getPerson()
 
+    //MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableView.dataSource = self
         tableView.rowHeight = 60
-        self.navigationItem.backButtonTitle = ""
         tableView.separatorStyle = .none
+        
+        self.navigationItem.backButtonTitle = ""
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -30,39 +35,21 @@ class InformationViewController: UIViewController {
     }
 }
 
-//MARK: - TableViewDataSource
-extension InformationViewController: UITableViewDataSource {
-    
+extension InformationViewController: UITableViewDataSource, UITableViewDelegate {
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         teamList.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        var content = cell.defaultContentConfiguration()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! PersonTableViewCell
         let person = teamList[indexPath.row]
-        content.text = person.fullName
-        content.textProperties.color = .init(
-            red: 0.221,
-            green: 0.209,
-            blue: 0.202,
-            alpha: 1
-        )
-        content.textProperties.font = UIFont(name: "Gilroy-Medium", size: 16)!
-        content.secondaryText = person.jobTitle
-        content.secondaryTextProperties.color = .init(
-            red: 0.67,
-            green: 0.67,
-            blue: 0.67,
-            alpha: 1
-        )
-        content.secondaryTextProperties.font = UIFont(name: "Gilroy-Medium", size: 14)!
-        content.image = UIImage(named: person.fullName)
-        content.imageToTextPadding = 16
-        content.imageProperties.maximumSize.height = 48
-        content.imageProperties.cornerRadius = 12
-        cell.contentConfiguration = content
+        cell.fullNameLabel.text = person.fullName
+        cell.jobTitleLabel.text = person.jobTitle
+        cell.photo.image = UIImage(named: person.fullName)
+        cell.photo.layer.cornerRadius = 12
+        
         return cell
     }
-    
+
 }
