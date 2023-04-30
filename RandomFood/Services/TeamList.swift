157a486b9e3d1f03e97000a506657102f4c36ac7
Vladimir <smile_friend@mail.ru>
2023-04-30 13:07:26 +0300

    Изменение описание разработчиков

--- RandomFood/Services/TeamList.swift
+++ RandomFood/Services/TeamList.swift
@@ -27,7 +27,7 @@ struct PersonStore {
                 age: "30",
                 town: "Якутск",
                 jobTitle: "iOS Developer",
-                bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                bio: "UX/UI дизайнер в региональном банке. В какой-то момент захотелось самому научится верстать на мобилке, поэтому я решил пойти на курсы Swiftbook"
             ),
             PersonStore(
                 name: "Владимир",
